bfb61832eec8298287cc9cc6bf340dca75b17c07
Matt Castillo <mattcastillo2010@gmail.com>
2024-11-24 12:22:28 -0500

    tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bobbin.app",
+  site: "https://bobbin.app/",
   integrations: [tailwind(), mdx(), sitemap()],
   output: "server",
   adapter: netlify(),
