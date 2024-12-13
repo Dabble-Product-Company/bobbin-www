007a066c7f851e59f4c93c93d7da155eedc6fbb4
Matt Castillo <mattcastillo2010@gmail.com>
2024-12-13 15:49:45 -0500

    Google analytics
    - Gtag.js
    - Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://bobbin.app/',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })],
   output: 'server',
   adapter: netlify(),
 });
