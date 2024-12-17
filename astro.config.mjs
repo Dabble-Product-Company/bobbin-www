495396ed852102e3869de6a63c709f3ef948836f
Matt Castillo <mattcastillo2010@gmail.com>
2024-12-17 13:20:47 -0500

    Moving to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://bobbin.app/',
+
   integrations: [tailwind(), mdx(), sitemap(), partytown({
     config: {
       forward: ['dataLayer.push']
     }
   })],
+
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare(),
 });
